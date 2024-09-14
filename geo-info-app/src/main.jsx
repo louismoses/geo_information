823cab148dcf44623301a519366c0cf42bf36a27
louis <louismoses.rait@gmail.com>
2024-09-14 22:39:39 +0800

    env: stop tracking

--- geo-info-app/src/main.jsx
+++ geo-info-app/src/main.jsx
@@ -4,9 +4,13 @@ import "./index.css";
 import router from "./routes/route";
 
 import { RouterProvider } from "react-router-dom";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
